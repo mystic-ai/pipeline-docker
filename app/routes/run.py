fc56623d97b6d627da9bef42d7ce0ecc34f1d5c8
paulh <pcj.hetherington@gmail.com>
2022-01-15 17:40:45 +0000

    testing pipeliens

--- app/routes/run.py
+++ app/routes/run.py
@@ -1,23 +1,31 @@
 import os
 import json
 
+from dill import loads
+
+from os import listdir
+from os.path import isfile, join
+
 from fastapi.routing import APIRouter
 
 from app.core.middlewares.RouteLogger import RouteLogger
 
 from pipeline.schemas.run import RunGet
+from pipeline.objects import Graph
 
 router = APIRouter(prefix="/run", route_class=RouteLogger)
 
 
-@router.post(
-    "/",
-)
-async def create(
-    run_data: RunGet,
-):
-    print("Got run req")
-
-    
-    
-    return {"": ""}
+pipeline_graph_files = [f for f in listdir("/app/pipelines") if isfile(join("/app/pipelines", f)) and f.endswith(".graph")]
+for file_str in pipeline_graph_files:
+    with open(join("/app/pipelines/", file_str)) as pipeline_graph_file:
+        pipeline_graph = loads(pipeline_graph_file.read())
+        if isinstance(pipeline_graph, Graph):
+            @router.post(
+                "/%s" % pipeline_graph.name,
+            )
+            async def create(
+                run_data,
+            ):
+                print("Got run req")
+                return {"result":pipeline_graph.run(run_data)}
